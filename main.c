0b5d7025418876fd0fa0a414de8f63bdb4794005
CaptainProton42 <john.wigg@gmx.net>
2019-04-10 13:23:02 +0200

    added main.c, memory.h, doxygen comment

---
+++ main.c
@@ -0,0 +1,23 @@
+/**
+ * @file main.c
+ * @author Hollweck, Wigg
+ * @date 10 April 2019
+ * @brief Main file.
+ *
+ * Here typically goes a more extensive explanation of what the header
+ * defines. Doxygens tags are words preceeded by either a backslash @\
+ * or by an at symbol @@.
+ * @see http://www.stack.nl/~dimitri/doxygen/docblocks.html
+ * @see http://www.stack.nl/~dimitri/doxygen/commands.html
+ */
+
+/**
+ * @brief Main function.
+ * 
+ * This is the main function.
+ * @return 0 on exit.
+ */
+
+int main() {
+    return 0;
+}
