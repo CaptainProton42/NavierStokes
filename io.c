dea9da2f521c70c779b0f8bad9315046624cf8ca
horropie <31204217+horropie@users.noreply.github.com>
2019-04-11 14:12:55 +0200

    More parameters in globals.

---
+++ io.c
@@ -0,0 +1,7 @@
+#include "io.h"
+#include "globals.h"
+
+double dimension_i = i_max + 1;
+double dimension_j = j_max + 1;
+
+double cells_number = dimensions_i * dimensions_j;
---
+++ io.h
@@ -0,0 +1,32 @@
+/**
+ * @file io.h
+ * @author Hollweck, Wigg
+ * @date 10 April 2019
+ * @brief In- and outputs.
+ *
+ * Here typically goes a more extensive explanation of what the header
+ * defines. Doxygens tags are words preceeded by either a backslash @\
+ * or by an at symbol @@.
+ * @see http://www.stack.nl/~dimitri/doxygen/docblocks.html
+ * @see http://www.stack.nl/~dimitri/doxygen/commands.html
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define Re /* Probably should go into main.c */
+#define T
+#define delt
+#define delx
+#define dely
+
+extern double i_max;
+extern double j_max;
+
+extern double dimension_i;
+extern double dimension_j;
+
+extern double cells_number;
+
+
+
