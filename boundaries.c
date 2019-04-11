8185ea6cd9dc1c2a37aeaef76c86c55875488fb6
CaptainProton42 <john.wigg@gmx.net>
2019-04-11 14:16:51 +0200

    implemented boundary conditions

--- boundaries.c
+++ boundaries.c
@@ -4,10 +4,39 @@ int set_noslip(double** u, double** v, int side) {
     return set_inflow(u, v, side, 0, 0);
 }
 
-int set_inflow(double** u, double** v, int side, double u_fox, double v_fix) {
-    int i, j;
+int set_inflow(double** u, double** v, int side, double u_fix, double v_fix) {
     switch(side) {
         case TOP:
-            for (i = 0; i < i_max+2; i++)
+            int i;
+            for (i = 0; i < i_max+2; i++) {
+                v[i][j_max] = v_fix; /* Set fixed values on border. */
+                u[i][i_max + 1] = 2 * u_fix - u[i][j_max]; /* Set values with no exact border value by averaging. */
+            }
+            break;
+        case BOTTOM:
+            int i;
+            for (i = 0; i < i_max+2; i++) {
+                v[i][0] = v_fix;
+                u[i][0] = 2 * u_fix - u[i][1];
+            }
+            break;
+        case LEFT:
+            int j;
+            for (j = 0; j < j_max+2; j++) {
+                u[0][j] = u_fix;
+                v[0][j] = 2 * v_fix  - v[1][j];
+            }
+            break;
+        case RIGHT:
+            int j;
+            for (j = 0; j < j_max+2; j++) {
+                u[i_max][j] = u_fix;
+                v[i_max+1][j] = 2 * v_fix - v[i_max][j];
+            }
+            break;
+        default: 
+            return -1;
     }
+
+    return 0;
 }
--- memory.h
+++ memory.h
@@ -3,12 +3,6 @@
  * @author Hollweck, Wigg
  * @date 10 April 2019
  * @brief Memory management.
- *
- * Here typically goes a more extensive explanation of what the header
- * defines. Doxygens tags are words preceeded by either a backslash @\
- * or by an at symbol @@.
- * @see http://www.stack.nl/~dimitri/doxygen/docblocks.html
- * @see http://www.stack.nl/~dimitri/doxygen/commands.html
  */
 
 #ifndef _MEMORY_H
