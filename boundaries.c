c91f2cdf21892a2b2ceee3fd4eaff91da0e67d95
horropie <31204217+horropie@users.noreply.github.com>
2019-04-17 14:54:42 +0200

    bugs in boundaries

--- boundaries.c
+++ boundaries.c
@@ -1,34 +1,35 @@
 #include "boundaries.h"
 
 int set_noslip(int i_max, int j_max, double** u, double** v, int side) {
-    return set_inflow(int i_max, int j_max, double ** u, double** v, side, 0, 0);
+    return set_inflow(i_max, j_max, u, v, side, 0, 0);
 }
 
 int set_inflow(int i_max, int j_max, double** u, double** v, int side, double u_fix, double v_fix) {
+    int i,j;
     switch(side) {
         case TOP:
-            int i;
+            
             for (i = 0; i < i_max+2; i++) {
                 v[i][j_max] = v_fix; /* Set fixed values on border. */
                 u[i][i_max + 1] = 2 * u_fix - u[i][j_max]; /* Set values with no exact border value by averaging. */
             }
             break;
         case BOTTOM:
-            /*int i;*/
+            
             for (i = 0; i < i_max+2; i++) {
                 v[i][0] = v_fix;
                 u[i][0] = 2 * u_fix - u[i][1];
             }
             break;
         case LEFT:
-            int j;
+            
             for (j = 0; j < j_max+2; j++) {
                 u[0][j] = u_fix;
                 v[0][j] = 2 * v_fix  - v[1][j];
             }
             break;
         case RIGHT:
-            /*int j;*/
+            
             for (j = 0; j < j_max+2; j++) {
                 u[i_max][j] = u_fix;
                 v[i_max+1][j] = 2 * v_fix - v[i_max][j];
